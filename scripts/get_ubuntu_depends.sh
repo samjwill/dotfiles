23d199090b37d7ec5bed80bcaae36aa1b7c694aa
Samuel Williams <25990267+SamuelWilliams256@users.noreply.github.com>
2022-05-11 12:41:11 -0400

    Update get_ubuntu_depends.sh

--- scripts/get_ubuntu_depends.sh
+++ scripts/get_ubuntu_depends.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install -y curl git ripgrep universal-ctags vim-gtk
+apt-get install -y bash-completion curl git ripgrep universal-ctags vim-gtk
 vim +'PlugInstall --sync' +qall > /dev/null
