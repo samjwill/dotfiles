c777e4e6ef9c8326fd72c3952c436abd03a597f8
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-05-22 13:58:51 -0400

    Update get_ubuntu_depends.sh

--- scripts/get_ubuntu_depends.sh
+++ scripts/get_ubuntu_depends.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install -y bash-completion curl git neovim ripgrep universal-ctags
+apt-get install -y bash-completion curl fzf git neovim ripgrep universal-ctags
 nvim --headless +PlugInstall +qall
