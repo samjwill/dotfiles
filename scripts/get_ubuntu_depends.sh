73153617fc7607f46041679dbbf9aa8bc6fa37f6
samjwill <25990267+samjwill@users.noreply.github.com>
2022-05-23 23:02:28 -0400

    Commented out broken command.

--- scripts/get_ubuntu_depends.sh
+++ scripts/get_ubuntu_depends.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get install -y bash-completion curl fzf git neovim ripgrep universal-ctags
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+# TODO: Not sure why this doesn't work...: nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
