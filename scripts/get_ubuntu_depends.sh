218f3e69e0ae5432364b41daf0f0540aaaf948b8
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-05-24 10:30:04 -0400

    Update get_ubuntu_depends.sh

--- scripts/get_ubuntu_depends.sh
+++ scripts/get_ubuntu_depends.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install -y bash-completion curl fzf git neovim ripgrep universal-ctags
+apt-get install -y bash-completion curl fd-find fzf git neovim ripgrep universal-ctags
 # TODO: Not sure why this doesn't work...: nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
