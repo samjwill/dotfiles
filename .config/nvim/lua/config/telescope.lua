2742f954f066c3a02aca3f2a1955c22bfd6c4674
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-05-24 22:40:22 -0400

    Update telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,6 +1,5 @@
 if packer_plugins["telescope.nvim"] and packer_plugins["telescope.nvim"].loaded then
 
---TODO: Unsure if these parenthesis are necessary or not?
 require("telescope").setup({
     defaults = {
         sorting_strategy = 'ascending',
