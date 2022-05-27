b1f08ac5ce014bb9860ea99d627e1f53b295c3de
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-05-27 08:45:44 -0400

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,7 +11,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
         'https://github.com/wbthomason/packer.nvim',
         install_path
     }
-    print "Installing Packer. Please close and reopen Neovim on install completion..."
+    print "Installing Packer. Please wait..."
     --TODO: Not certain that this is the best way to do this, but if we don't, Packer isn't recognized. Also saw a solution which modifies the path Neovim searches, so that might be worth looking into.
     vim.cmd("packadd packer.nvim")
 end
@@ -32,7 +32,8 @@ return require('packer').startup(function(use)
     -- TODO: Running the following two commands after Packer install completion
     -- allows for telescope and treesitter to be used immediately on first
     -- launch. Investigate why and see if something can be implemented into the
-    -- init.vim file to allow for the same behavior.
+    -- init.vim file to allow for the same behavior. Might also be
+    -- related to the packadd packer.nvim call above.
     --
     --runtime! plugin/**/*.vim
     --runtime! plugin/**/*.lua
@@ -57,7 +58,10 @@ return require('packer').startup(function(use)
 
     -- Automatically set up your configuration after cloning packer.nvim
     if packer_bootstrap then
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "PackerComplete",
+			callback = function() print("Installation is complete! Please restart Neovim.") end
+		})
         require('packer').sync()
     end
 end)
-
