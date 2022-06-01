94b3a07cdcbdbdd45f3f170fc93be93a3b96a19f
samjwill <25990267+samjwill@users.noreply.github.com>
2022-05-31 23:52:59 -0400

    Simplified keybinds.

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,37 +1,19 @@
 -- TODO: Group these keybinds where the rest of the keybinds are defined.
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<space>le', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>l[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>l]d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>lq', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>le',     '<cmd>lua vim.diagnostic.open_float()<CR>',   opts)
+vim.api.nvim_set_keymap('n', '<space>l[d',    '<cmd>lua vim.diagnostic.goto_prev()<CR>',    opts)
+vim.api.nvim_set_keymap('n', '<space>l]d',    '<cmd>lua vim.diagnostic.goto_next()<CR>',    opts)
+vim.api.nvim_set_keymap('n', '<space>lq',     '<cmd>lua vim.diagnostic.setloclist()<CR>',   opts)
+vim.api.nvim_set_keymap('n', '<space>lgD',    '<cmd>lua vim.lsp.buf.declaration()<CR>',     opts)
+vim.api.nvim_set_keymap('n', '<space>lgd',    '<cmd>lua vim.lsp.buf.definition()<CR>',      opts)
+vim.api.nvim_set_keymap('n', '<space>lK',     '<cmd>lua vim.lsp.buf.hover()<CR>',           opts)
+vim.api.nvim_set_keymap('n', '<space>lgi',    '<cmd>lua vim.lsp.buf.implementation()<CR>',  opts)
+vim.api.nvim_set_keymap('n', '<space>l<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',  opts)
+vim.api.nvim_set_keymap('n', '<space>ltd',    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>lrn',    '<cmd>lua vim.lsp.buf.rename()<CR>',          opts)
+vim.api.nvim_set_keymap('n', '<space>lca',    '<cmd>lua vim.lsp.buf.code_action()<CR>',     opts)
+vim.api.nvim_set_keymap('n', '<space>lgr',    '<cmd>lua vim.lsp.buf.references()<CR>',      opts)
+vim.api.nvim_set_keymap('n', '<space>lf',     '<cmd>lua vim.lsp.buf.formatting()<CR>',      opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  -- TODO: Look into this as well.
-  --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lgD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lgd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lK', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lgi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>l<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ltd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lgr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
-
-require('lspconfig').clangd.setup{
-    on_attach = on_attach
-}
+require('lspconfig').clangd.setup{}
