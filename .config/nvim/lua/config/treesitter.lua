ce1696eb1f418ad414983ce094d7218e4fdf12fb
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-09-13 13:17:15 -0400

    Update treesitter.lua

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -9,4 +9,4 @@ require("nvim-treesitter.configs").setup({
 
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 9 -- Same as set by pressing `zR`
-vim.opt.foldmethod= "expr"
+vim.opt.foldmethod = "expr"
