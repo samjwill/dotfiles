e6c1649e389c9c1ecf760a94635739a1ab0b95f3
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-13 15:54:23 -0400

    Update hop.lua

--- .config/nvim/lua/config/hop.lua
+++ .config/nvim/lua/config/hop.lua
@@ -4,8 +4,8 @@ require'hop'.setup {
     keys = 'etovxqpdygfblzhckisuran'
 }
 
-vim.api.nvim_set_keymap('n', 's', '<CMD>HopWordMW<CR>',    opts)
+vim.api.nvim_set_keymap('n', 's', '<CMD>HopWordMW<CR>', opts)
 vim.cmd("autocmd FileType netrw nnoremap <buffer> s <CMD>HopWordMW<CR>")
 
-vim.api.nvim_set_keymap('n', 'S', '<CMD>HopChar1MW<CR>',    opts)
+vim.api.nvim_set_keymap('n', 'S', '<CMD>HopChar1MW<CR>', opts)
 vim.cmd("autocmd FileType netrw nnoremap <buffer> S <CMD>HopChar1MW<CR>")
