181e40896f932b8e9a2dddd6f0f5177f86df8857
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-14 19:16:35 -0400

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -96,6 +96,12 @@ require('packer').startup(function(use)
 
     use
     {
+        'numToStr/Comment.nvim',
+        config = function() require('Comment').setup{} end
+    }
+
+    use
+    {
         'echasnovski/mini.nvim',
         config = "require('config.mini')"
     }
