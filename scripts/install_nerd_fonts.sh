4a99f5d96f44321335da6fc6c74c743c4940dcf9
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-18 19:15:14 -0400

    Update install_nerd_fonts.sh

--- scripts/install_nerd_fonts.sh
+++ scripts/install_nerd_fonts.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-curl -fLo Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip && mkdir -p ~/.local/share/fonts/ && unzip Hack.zip -d ~/.local/share/fonts/
+curl -fLo /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip && mkdir -p ~/.local/share/fonts/ && unzip /tmp/Hack.zip -d ~/.local/share/fonts/ && rm /tmp/Hack.zip
