0803e4963f6e9c50e909f75dbf5f45ee01f1be67
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-20 17:28:05 -0400

    Update gruvbox.lua

--- .config/nvim/lua/config/gruvbox.lua
+++ .config/nvim/lua/config/gruvbox.lua
@@ -1,15 +1,7 @@
 vim.cmd([[
     try
-        colorscheme gruvbox
-
-        "TODO: see if you can use named colors instead of hard-coding hex codes
-        augroup INIT_STATUSLINE_COLORS | autocmd!
-            "Toggle new color when in insert mode
-            autocmd InsertEnter * highlight StatusLine ctermfg=108 ctermbg=237 guifg='#8ec07c' guibg='#3c3836'
-            autocmd InsertLeave * highlight StatusLine ctermfg=239 ctermbg=223 guifg='#504945' guibg='#ebdbb2'
-            autocmd TermEnter * highlight StatusLine ctermfg=108 ctermbg=237 guifg='#8ec07c' guibg='#3c3836'
-            autocmd TermLeave * highlight StatusLine ctermfg=239 ctermbg=223 guifg='#504945' guibg='#ebdbb2'
-        augroup end
+        let g:gruvbox_material_foreground='original'
+        colorscheme gruvbox-material
     catch
     endtry
 ]])
