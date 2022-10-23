968d97486a3bec7e75ccd799a2548b2a38d0a302
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-23 16:17:22 -0400

    More mappings.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,35 +69,35 @@ vim.g.netrw_list_hide = vim.g.netrw_list_hide .. "^\\.\\./"
 -- TODO: Sort these mappings more logically.
 vim.g.mapleader = " "
 
+-- Normal, Visual, Select, Operator-pending Modes
+
+    -- Don't want to :set ignorecase because * and # should be case-sensitive
+    vim.api.nvim_set_keymap("", "/", "/\\c", { noremap=true})
+    vim.api.nvim_set_keymap("", "?", "?\\c", { noremap=true})
+    vim.api.nvim_set_keymap("", "<Leader>/", "/", { noremap=true})
+    vim.api.nvim_set_keymap("", "<Leader>?", "?", { noremap=true})
+
+    -- "-" to edit directory of current file
+    -- TODO: disallow for terminal buffers
+    vim.api.nvim_set_keymap("", "-", ":e %:p:h<CR>", { noremap=true, silent=true })
+
+    -- Bufdir Plugin
+    -- Open command-line
+    vim.api.nvim_set_keymap("", "<Leader>cc", ":Bufdir terminal<CR>i", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>cC", ":terminal<CR>i", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>cs", ":split | Bufdir terminal<CR>i", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>cS", ":split | terminal<CR>i", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>cv", ":vsplit | Bufdir terminal<CR>i", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>cV", ":vsplit | terminal<CR>i", { noremap=true, silent=true })
+
+    -- Telescope Plugin
+    vim.api.nvim_set_keymap("", "<Leader>t/", ":Telescope current_buffer_fuzzy_find<CR>", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>tf", ":Telescope find_files<CR>", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>tl", ":Telescope live_grep<CR>", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("", "<Leader>tt", ":Telescope<CR>", { noremap=true, silent=true })
+
 -- TODO: Port the below to Lua
 vim.cmd([[
-"Normal, Visual, Select, Operator-pending Modes
-
-    "Don't want to :set ignorecase because * and # should be case-sensitive
-    lua vim.api.nvim_set_keymap("", "/", "/\\c", { noremap=true})
-    lua vim.api.nvim_set_keymap("", "?", "?\\c", { noremap=true})
-    lua vim.api.nvim_set_keymap("", "<Leader>/", "/", { noremap=true})
-    lua vim.api.nvim_set_keymap("", "<Leader>?", "?", { noremap=true})
-
-    "- to edit directory of current file
-    "TODO: disallow for terminal buffers
-    lua vim.api.nvim_set_keymap("", "-", ":e %:p:h<CR>", { noremap=true, silent=true })
-
-    "Bufdir Plugin
-    "Open command-line
-    lua vim.api.nvim_set_keymap("", "<Leader>cc", ":Bufdir terminal<CR>i", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>cC", ":terminal<CR>i", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>cs", ":split | Bufdir terminal<CR>i", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>cS", ":split | terminal<CR>i", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>cv", ":vsplit | Bufdir terminal<CR>i", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>cV", ":vsplit | terminal<CR>i", { noremap=true, silent=true })
-
-    "Telescope Plugin
-    lua vim.api.nvim_set_keymap("", "<Leader>t/", ":Telescope current_buffer_fuzzy_find<CR>", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>tf", ":Telescope find_files<CR>", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>tl", ":Telescope live_grep<CR>", { noremap=true, silent=true })
-    lua vim.api.nvim_set_keymap("", "<Leader>tt", ":Telescope<CR>", { noremap=true, silent=true })
-
 "Insert Mode
 
     "shift-tab to inverse tab
