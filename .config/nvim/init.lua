a9fbc273f53dc53dd33c82c2e40e0a34293c6288
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-29 19:05:20 -0400

    Moved autocommands to their own file.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,21 +6,6 @@
 
 require("settings")
 require("keymaps")
-
--- Autocommands
-    local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
-
-    -- Turn off line numbers in terminal emulator
-    vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
-
-    -- Automatically close terminal buffer when done.
-    -- TODO: This breaks the :ter command.
-    vim.api.nvim_create_autocmd("TermClose", {group = init_group,command = "if !v:event.status | execute 'bdelete! ' . expand('<abuf>') | endif"})
-
---------------------------------------------------------------------------------
---                                 Plugins                                    --
---                                                                            --
---------------------------------------------------------------------------------
-
+require("autocommands")
 require("plugins")
 
