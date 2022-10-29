edff026547eef1f6b1679b7c63ffe913e6e7b82e
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-29 19:46:42 -0400

    Removed comment.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- TODO - list of things that don't work quite right and need to be addressed.
 -- Using `:ter <my_command>` without automatically closing buffer.
--- Terminal buffer being deleted when it's still visible using unception
 -- Termdebug does not always follow highlighting
 -- Hop not working when terminal buffer has focus or when inside of commandline window
 
