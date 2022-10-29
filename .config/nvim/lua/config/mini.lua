39412be6ae3eed5ee772c96913ebbaa59ce7dd72
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-29 13:08:53 -0400

    Update mini.lua

--- .config/nvim/lua/config/mini.lua
+++ .config/nvim/lua/config/mini.lua
@@ -1,3 +1,7 @@
 require('mini.completion').setup{
     fallback_action = function() end -- None. Only auto-complete with LSP.
 }
+
+require('mini.cursorword').setup{}
+
+require('mini.comment').setup{}
