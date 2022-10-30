850ae50c3d73cb8195c6e42763b69ed7b599e2e3
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-29 21:48:25 -0400

    Replaced : with <CMD>

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -18,16 +18,16 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-d>", { noremap=true, silent=true })
 --     in Settings>Keyboard Shortcuts and then bind:
 --         Backtab+Ctrl+Ansi -> \E[27;6;9~
 --         Tab+Ctrl+Ansi     -> \E[27;5;9~
-vim.api.nvim_set_keymap("n", "<C-Tab>",   ":tabn<CR>",            {noremap=true, silent=true})
-vim.api.nvim_set_keymap("c", "<C-Tab>",   "<C-\\><C-N>:tabn<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("i", "<C-Tab>",   "<C-\\><C-N>:tabn<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("t", "<C-Tab>",   "<C-\\><C-N>:tabn<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("v", "<C-Tab>",   "<C-\\><C-N>:tabn<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("n", "<C-S-Tab>", ":tabp<CR>",            {noremap=true, silent=true})
-vim.api.nvim_set_keymap("c", "<C-S-Tab>", "<C-\\><C-N>:tabp<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("i", "<C-S-Tab>", "<C-\\><C-N>:tabp<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("t", "<C-S-Tab>", "<C-\\><C-N>:tabp<CR>", {noremap=true, silent=true})
-vim.api.nvim_set_keymap("v", "<C-S-Tab>", "<C-\\><C-N>:tabp<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<C-Tab>",   "<CMD>tabn<CR>",            {noremap=true, silent=true})
+vim.api.nvim_set_keymap("c", "<C-Tab>",   "<C-\\><C-N><CMD>tabn<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("i", "<C-Tab>",   "<C-\\><C-N><CMD>tabn<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("t", "<C-Tab>",   "<C-\\><C-N><CMD>tabn<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("v", "<C-Tab>",   "<C-\\><C-N><CMD>tabn<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<C-S-Tab>", "<CMD>tabp<CR>",            {noremap=true, silent=true})
+vim.api.nvim_set_keymap("c", "<C-S-Tab>", "<C-\\><C-N><CMD>tabp<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("i", "<C-S-Tab>", "<C-\\><C-N><CMD>tabp<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("t", "<C-S-Tab>", "<C-\\><C-N><CMD>tabp<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("v", "<C-S-Tab>", "<C-\\><C-N><CMD>tabp<CR>", {noremap=true, silent=true})
 
 -- Escape to escape a terminal
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap=true })
@@ -51,22 +51,22 @@ vim.cmd("autocmd FileType netrw nnoremap <buffer> S <CMD>HopChar2MW<CR>")
 -------------------------------------------------------------------------------
 local lsp_leader = vim.g.mapleader.."l"
 
-vim.api.nvim_set_keymap('n', lsp_leader..'[', '<cmd>lua vim.diagnostic.goto_prev()<CR>',    opts)
-vim.api.nvim_set_keymap('n', lsp_leader..']', '<cmd>lua vim.diagnostic.goto_next()<CR>',    opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'c', '<cmd>lua vim.lsp.buf.code_action()<CR>',     opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'d', '<cmd>lua vim.lsp.buf.definition()<CR>',      opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'D', '<cmd>lua vim.lsp.buf.declaration()<CR>',     opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'f', '<cmd>lua vim.lsp.buf.formatting()<CR>',      opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'i', '<cmd>lua vim.lsp.buf.implementation()<CR>',  opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'r', '<cmd>lua vim.lsp.buf.references()<CR>',      opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'e', '<cmd>lua vim.diagnostic.open_float()<CR>',   opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'k', '<cmd>lua vim.lsp.buf.hover()<CR>',           opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'K', '<cmd>lua vim.lsp.buf.signature_help()<CR>',  opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'q', '<cmd>lua vim.diagnostic.setloclist()<CR>',   opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'s', '<cmd>lua vim.lsp.buf.rename()<CR>',          opts)
-vim.api.nvim_set_keymap('n', lsp_leader..'t', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'[', '<CMD>lua vim.diagnostic.goto_prev()<CR>',    opts)
+vim.api.nvim_set_keymap('n', lsp_leader..']', '<CMD>lua vim.diagnostic.goto_next()<CR>',    opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'c', '<CMD>lua vim.lsp.buf.code_action()<CR>',     opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'d', '<CMD>lua vim.lsp.buf.definition()<CR>',      opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'D', '<CMD>lua vim.lsp.buf.declaration()<CR>',     opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'f', '<CMD>lua vim.lsp.buf.formatting()<CR>',      opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'i', '<CMD>lua vim.lsp.buf.implementation()<CR>',  opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'r', '<CMD>lua vim.lsp.buf.references()<CR>',      opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'e', '<CMD>lua vim.diagnostic.open_float()<CR>',   opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'k', '<CMD>lua vim.lsp.buf.hover()<CR>',           opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'K', '<CMD>lua vim.lsp.buf.signature_help()<CR>',  opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'q', '<CMD>lua vim.diagnostic.setloclist()<CR>',   opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'s', '<CMD>lua vim.lsp.buf.rename()<CR>',          opts)
+vim.api.nvim_set_keymap('n', lsp_leader..'t', '<CMD>lua vim.lsp.buf.type_definition()<CR>', opts)
 
-clangd_keymaps = vim.api.nvim_set_keymap('n', lsp_leader..'a', '<cmd>ClangdSwitchSourceHeader<CR>', opts)
+clangd_keymaps = vim.api.nvim_set_keymap('n', lsp_leader..'a', '<CMD>ClangdSwitchSourceHeader<CR>', opts)
 
 
 -------------------------------------------------------------------------------
@@ -74,12 +74,12 @@ clangd_keymaps = vim.api.nvim_set_keymap('n', lsp_leader..'a', '<cmd>ClangdSwitc
 -------------------------------------------------------------------------------
 local command_line_leader = vim.g.mapleader.."c"
 
-vim.api.nvim_set_keymap("", command_line_leader.."c", ":Bufdir terminal<CR>i", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", command_line_leader.."C", ":terminal<CR>i", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", command_line_leader.."s", ":split | Bufdir terminal<CR>i", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", command_line_leader.."S", ":split | terminal<CR>i", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", command_line_leader.."v", ":vsplit | Bufdir terminal<CR>i", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", command_line_leader.."V", ":vsplit | terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."c", "<CMD>Bufdir terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."C", "<CMD>terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."s", "<CMD>split | Bufdir terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."S", "<CMD>split | terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."v", "<CMD>vsplit | Bufdir terminal<CR>i", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", command_line_leader.."V", "<CMD>vsplit | terminal<CR>i", { noremap=true, silent=true })
 
 
 -------------------------------------------------------------------------------
@@ -87,10 +87,10 @@ vim.api.nvim_set_keymap("", command_line_leader.."V", ":vsplit | terminal<CR>i",
 -------------------------------------------------------------------------------
 local telescope_leader = vim.g.mapleader.."t"
 
-vim.api.nvim_set_keymap("", telescope_leader.."/", ":Telescope current_buffer_fuzzy_find<CR>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", telescope_leader.."f", ":Telescope find_files<CR>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", telescope_leader.."l", ":Telescope live_grep<CR>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("", telescope_leader.."t", ":Telescope<CR>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", telescope_leader.."/", "<CMD>Telescope current_buffer_fuzzy_find<CR>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", telescope_leader.."f", "<CMD>Telescope find_files<CR>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", telescope_leader.."l", "<CMD>Telescope live_grep<CR>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("", telescope_leader.."t", "<CMD>Telescope<CR>", { noremap=true, silent=true })
 
 
 -------------------------------------------------------------------------------
