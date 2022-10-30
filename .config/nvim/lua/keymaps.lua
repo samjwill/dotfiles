853cca97d8fcf491ea1e7eefedb3a2dc7f5d945f
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-30 12:38:34 -0400

    Added more descriptions.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -73,12 +73,12 @@ clangd_keymaps = vim.keymap.set('n', lsp_leader..'a', '<CMD>ClangdSwitchSourceHe
 -------------------------------------------------------------------------------
 local command_line_leader = vim.g.mapleader.."c"
 
-vim.keymap.set("", command_line_leader.."c", "<CMD>Bufdir terminal<CR>i", {})
-vim.keymap.set("", command_line_leader.."C", "<CMD>terminal<CR>i", {})
-vim.keymap.set("", command_line_leader.."s", "<CMD>split | Bufdir terminal<CR>i", {})
-vim.keymap.set("", command_line_leader.."S", "<CMD>split | terminal<CR>i", {})
-vim.keymap.set("", command_line_leader.."v", "<CMD>vsplit | Bufdir terminal<CR>i", {})
-vim.keymap.set("", command_line_leader.."V", "<CMD>vsplit | terminal<CR>i", {})
+vim.keymap.set("", command_line_leader.."c", "<CMD>Bufdir terminal<CR>i", {desc = "Open Terminal in Current Buffer's Dir"})
+vim.keymap.set("", command_line_leader.."C", "<CMD>terminal<CR>i", {desc = "Open Terminal"})
+vim.keymap.set("", command_line_leader.."s", "<CMD>split | Bufdir terminal<CR>i", {desc = "Split Terminal in Current Buffer's Dir"})
+vim.keymap.set("", command_line_leader.."S", "<CMD>split | terminal<CR>i", {desc = "Split Terminal"})
+vim.keymap.set("", command_line_leader.."v", "<CMD>vsplit | Bufdir terminal<CR>i", {desc = "VSplit Terminal in Current Buffer's Dir"})
+vim.keymap.set("", command_line_leader.."V", "<CMD>vsplit | terminal<CR>i", {desc = "VSplit Terminal"})
 
 
 -------------------------------------------------------------------------------
