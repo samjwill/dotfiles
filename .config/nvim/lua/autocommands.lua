20a153869eace194bf4acbc650917b5b461f660a
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-29 21:05:02 -0400

    Removed TODO.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -4,6 +4,5 @@ local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
 vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
 
 -- Automatically close terminal buffer when done.
--- TODO: This breaks the :ter command.
 vim.api.nvim_create_autocmd("TermClose", {group = init_group,command = "if !v:event.status | execute 'bdelete! ' . expand('<abuf>') | endif"})
 
