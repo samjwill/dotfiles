d5bdacf2f9660870417b863398c70465f89a2fd3
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-29 20:10:38 -0400

    Removed more TODOs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,5 @@
--- TODO - list of things that don't work quite right and need to be addressed.
--- Using `:ter <my_command>` without automatically closing buffer.
--- Termdebug does not always follow highlighting
--- Hop not working when terminal buffer has focus or when inside of commandline window
-
 require("settings")
 require("keymaps")
 require("autocommands")
 require("plugins.plugins")
+
