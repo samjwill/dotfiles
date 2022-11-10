7c7b8a776650d47405e055b2f1414f4d9e6198a6
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-11-10 15:44:23 -0500

    Update install_neovim_plugins.sh

--- scripts/install_neovim_plugins.sh
+++ scripts/install_neovim_plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # No idea if this is necessary?
+# I think this allows Packer to be cloned during the bootstrapping phase? Honestly not sure.
 nvim --headless +qa
 
 # Need to PackerSync first so that subsequent command is recognized...?
