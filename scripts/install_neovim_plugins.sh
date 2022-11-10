082665dd243511d49794fb1cc758536dbdb6ed5e
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-11-10 15:42:29 -0500

    Update install_neovim_plugins.sh

--- scripts/install_neovim_plugins.sh
+++ scripts/install_neovim_plugins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# No idea if this is necessary?
+nvim --headless +qa
+
 # Need to PackerSync first so that subsequent command is recognized...?
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless +TSUpdateSync +qa
