32a7875061d74d8eb2a989629e1bfb593658acc9
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-11-10 16:02:23 -0500

    Update install_neovim_plugins.sh

--- scripts/install_neovim_plugins.sh
+++ scripts/install_neovim_plugins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# No idea why this is necessary? This doesn't work without it.
+# No idea if this is necessary?
 # I think this allows Packer to be cloned during the bootstrapping phase? Honestly not sure.
 nvim --headless +qa
 
 # Need to PackerSync first so that subsequent command is recognized...?
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless +TSUpdateSync +qa
+nvim --headless -c "TSUpdateSync" -c "quitall"
+nvim --headless -c "MasonInstall clangd" -c "quitall" # If updating this, update the ensure_installed in Neovim init.lua
