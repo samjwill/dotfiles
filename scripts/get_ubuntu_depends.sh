325134938277696b5eaf914fc911b4ea9fc8f86b
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-11-10 14:33:16 -0500

    Update get_ubuntu_depends.sh

--- scripts/get_ubuntu_depends.sh
+++ scripts/get_ubuntu_depends.sh
@@ -2,3 +2,7 @@
 
 apt-get update
 apt-get install -y bash-completion clangd curl g++ git fd-find fzf git gzip ripgrep unzip tar wget
+
+wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb -P /tmp/
+yes | apt install /tmp/nvim-linux64.deb || true
+rm /tmp/nvim-linux64.deb
