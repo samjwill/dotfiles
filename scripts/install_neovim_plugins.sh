fe9294ee813255e51fca8066f04bc30d525e64f1
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-11-11 10:52:40 -0500

    Update install_neovim_plugins.sh

--- scripts/install_neovim_plugins.sh
+++ scripts/install_neovim_plugins.sh
@@ -7,4 +7,4 @@ nvim --headless +qa
 # Need to PackerSync first so that subsequent command is recognized...?
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless -c "TSUpdateSync" -c "quitall"
-nvim --headless -c "MasonInstall clangd" -c "quitall" # If updating this, update the ensure_installed in Neovim init.lua
+nvim --headless -c "MasonInstallAll" -c "quitall" # If updating this, update the ensure_installed in Neovim init.lua
