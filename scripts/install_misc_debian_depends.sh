f6e28b3e30b6bfeb6f7ca87c4a2419a9b51c367b
samjwill <25990267+samjwill@users.noreply.github.com>
2022-11-12 12:18:27 -0500

    Removed redundant depend.

--- scripts/install_misc_debian_depends.sh
+++ scripts/install_misc_debian_depends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y bash-completion clangd curl g++ git fd-find fzf git gzip ripgrep unzip tar wget
+apt-get install -y bash-completion curl g++ git fd-find fzf git gzip ripgrep unzip tar wget
 
