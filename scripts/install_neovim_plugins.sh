df0a75a9e2932e4354e31950be68704fe403bf5b
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-12-21 12:35:06 -0500

    Update install script so that it works with Lazy instead of Packer

--- scripts/install_neovim_plugins.sh
+++ scripts/install_neovim_plugins.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# I think this allows Packer to be cloned during the bootstrapping phase? Honestly not sure.
-nvim --headless +qa
-
-# Need to allow PackerSync to run first and then restart so that subsequent TSUpdate command is recognized...?
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless -c "TSUpdateSync" -c "MasonInstallAll" -c "quitall"
 
 echo ""
