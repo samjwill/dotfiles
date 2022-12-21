01bc4ee0dd999eefa9adfe322e909a9deed502e0
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-12-21 11:26:59 -0500

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,14 +13,22 @@ end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-    "psliwka/vim-smoothie",
-    "samjwill/vim-bufdir",
-    "samjwill/vim-auto-nohlsearch",
+    {
+        "psliwka/vim-smoothie"
+    },
+
+    {
+        "samjwill/vim-bufdir"
+    },
+
+    {
+        "samjwill/vim-auto-nohlsearch"
+    },
 
     {
         "lukas-reineke/indent-blankline.nvim",
         config = function()
-            require('plugin_config.indent-blankline')
+            require("plugin_config.indent-blankline")
         end
     },
 
@@ -28,7 +36,7 @@ require("lazy").setup({
         "nvim-telescope/telescope.nvim",
         dependencies = "nvim-lua/plenary.nvim",
         config = function()
-            require('plugin_config.telescope')
+            require("plugin_config.telescope")
         end
     },
 
@@ -46,7 +54,7 @@ require("lazy").setup({
     {
         "sainnhe/gruvbox-material",
         config = function()
-            require('plugin_config.gruvbox-material')
+            require("plugin_config.gruvbox-material")
         end
     },
 
@@ -62,7 +70,7 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         commit = "287ffdccc1dd7ed017d844a4fad069fd3340fa94",
         config = function()
-            require('plugin_config.treesitter')
+            require("plugin_config.treesitter")
         end,
         -- :TSUpdate fails when bootstrapping. Call update function directly.
         -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3135
@@ -81,24 +89,24 @@ require("lazy").setup({
     },
 
     {
-        'lewis6991/gitsigns.nvim',
+        "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup()
+            require("gitsigns").setup()
         end
     },
 
     {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
+        "nvim-lualine/lualine.nvim",
+        dependencies = {"kyazdani42/nvim-web-devicons", lazy = true},
         config = function()
-            require('lualine').setup({
+            require("lualine").setup({
                 options = {
-                    section_separators = { left = '', right = '' },
-                    component_separators = '',
+                    section_separators = { left = "", right = "" },
+                    component_separators = "",
                 },
                 sections = {
                     lualine_c = {
-                        {'filename', path = 3},
+                        {"filename", path = 3},
                     },
                 }
             })
@@ -106,27 +114,27 @@ require("lazy").setup({
     },
 
     {
-        'numToStr/Comment.nvim',
-        config = function() require('Comment').setup{} end
+        "numToStr/Comment.nvim",
+        config = function() require("Comment").setup{} end
     },
 
     {
-        'echasnovski/mini.nvim',
+        "echasnovski/mini.nvim",
         config = function()
-            -- require('mini.cursorword').setup{} -- TODO: Seems to break highlighting?
-            require('plugin_config.mini_completion')
-            require('plugin_config.mini_map')
+            -- require("mini.cursorword").setup{} -- TODO: Seems to break highlighting?
+            require("plugin_config.mini_completion")
+            require("plugin_config.mini_map")
         end
     },
 
     {
         "williamboman/mason.nvim",
-        config = function() require('mason').setup() end
+        config = function() require("mason").setup() end
     },
 
     {
         "williamboman/mason-lspconfig.nvim",
-        -- after = 'mason.nvim',
+        dependencies = {"williamboman/mason.nvim"},
         config = function()
             -- TODO: May be able to merge this with other clangd-specific tables for LSP?
             local package_list = { "clangd" }
@@ -145,18 +153,17 @@ require("lazy").setup({
 
     {
         "neovim/nvim-lspconfig",
-        -- after = 'mason-lspconfig.nvim',
+        dependencies = {"williamboman/mason-lspconfig.nvim"},
         config = function()
             local on_attach_func = function(client, bufnr)
-                vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+                vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
             end
 
             -- If changing these, update the values in mason-lspconfig as well as in the headless install neovim plugins script.
-            require('lspconfig').clangd.setup{
+            require("lspconfig").clangd.setup{
                 clangd_keymaps, -- Defined in keymaps file.
                 on_attach = on_attach_func
             }
         end
     }
 })
-
