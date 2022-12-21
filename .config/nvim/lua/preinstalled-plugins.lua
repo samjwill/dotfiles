3aca290bbb0726b2071e8a523e51c2a018745eea
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-12-21 14:38:57 -0500

    Update preinstalled-plugins.lua

--- .config/nvim/lua/preinstalled-plugins.lua
+++ .config/nvim/lua/preinstalled-plugins.lua
@@ -9,7 +9,7 @@ vim.g.termdebug_wide = 1
 -- Netrw
 -------------------------------------------------------------------------------
 vim.g.netrw_banner = 0
-vim.g.netrw_bufsettings = "noma nomod number nobl nowrap ro" -- All but "number" are default.
+-- vim.g.netrw_bufsettings = "noma nomod number nobl nowrap ro" -- All but "number" are default.
 
 -- Hide . and .. directories.
 vim.g.netrw_list_hide = ""
