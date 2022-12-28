3162668e393f700de6777d5e07ea0800eb967282
samjwill <25990267+samjwill@users.noreply.github.com>
2022-12-27 20:29:44 -0500

    Updated install script

--- scripts/install_misc_debian_depends.sh
+++ scripts/install_misc_debian_depends.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y bash-completion curl g++ git fd-find fzf git gzip ripgrep unzip tar wget
-
+apt-get install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
+# pacman -Syu
+# pacman -S bash-completion curl fd fzf gcc git gzip ripgrep unzip tar wget
