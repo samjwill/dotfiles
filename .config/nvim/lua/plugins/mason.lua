d4b2ffe58e09d1fde2c07375637a31589a051614
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-01-26 17:30:47 -0500

    Fix comments

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,6 +2,7 @@ local mason_package_list = { "clangd", "cpptools" }
 
 return {
     "williamboman/mason.nvim",
+    -- "build" is executed when a plugin is installed or updated.
     build = function()
         -- TODO: This causes GUI weirdness if Neovim is currently being
         -- bootstrapped. Leaving in place because at least this installs
@@ -9,10 +10,11 @@ return {
         -- already.
         vim.cmd("MasonInstall "..table.concat(mason_package_list, " "))
     end,
+    -- "config" is executed when the plugin loads.
     config = function()
         require("mason").setup()
 
-        -- Create an autocmd that allows for simple headless installation.
+        -- Create a command that allows for simple, headless installation.
         vim.api.nvim_create_user_command("MasonInstallAll", function ()
             vim.cmd("MasonInstall "..table.concat(mason_package_list, " "))
         end, {})
