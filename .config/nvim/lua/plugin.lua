6159f5d58a92f364eb361e1acc5a86ef9f217664
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-03-10 19:50:13 -0500

    Update plugin.lua

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -1,5 +1,5 @@
 -- Automatically install Lazy on startup.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
