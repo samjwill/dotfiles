6ed2b79730f767a1d107136c73948a5826394a73
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-03-21 14:55:50 -0400

    Update options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -29,9 +29,9 @@ vim.opt.showbreak = "..."
 vim.opt.breakindent = true
 vim.opt.copyindent = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth=4
-vim.opt.softtabstop=4
-vim.opt.tabstop=4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 
 -- Wildmenu
 vim.opt.wildignorecase = true
