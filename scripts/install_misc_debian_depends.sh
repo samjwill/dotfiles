31edf1c8884aa7a5e43f5d075a399a9e2cc7ec64
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-04-11 16:51:37 -0400

    Update install_misc_debian_depends.sh

--- scripts/install_misc_debian_depends.sh
+++ scripts/install_misc_debian_depends.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
 # pacman -Syu
-# pacman -S bash-completion curl fd fzf gcc git gzip ripgrep unzip tar wget
+# pacman -S bash-completion curl fd fzf gcc git git-delta gzip ripgrep unzip tar wget
