8c7c1d9bd5aadd9afe6e10ec59547afcba16a008
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-20 23:19:24 -0400

    Added TODOs.

--- scripts/install_depends.sh
+++ scripts/install_depends.sh
@@ -3,11 +3,12 @@
 # Check if package manager is pacman
 if [ "$(command -v pacman)" ]; then
   sudo pacman -Syu --noconfirm
-  sudo pacman -S --noconfirm bash-completion curl fd fzf gcc git git-delta gzip ripgrep unzip tar wget
+  sudo pacman -S --noconfirm bash-completion curl fd fzf gcc git git-delta gzip neovim ripgrep unzip tar wget
 # Check if package manager is apt
 elif [ "$(command -v apt)" ]; then
   sudo apt update
   sudo apt install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
+  # TODO: Neovim, git-delta
 else
   echo "Unknown package manager"
   exit 1
