acf6f2a7960bf88790bd1df1c0178397672c6d96
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-20 23:27:56 -0400

    Merged install depends scripts.

--- scripts/install_depends.sh
+++ scripts/install_depends.sh
@@ -13,3 +13,11 @@ else
   echo "Unknown package manager"
   exit 1
 fi
+
+# Install nerd font to fonts directory in home folder.
+curl -fLo /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip && \
+    mkdir -p ~/.local/share/fonts/ && \
+    unzip /tmp/Hack.zip -d ~/.local/share/fonts/ && \
+    rm /tmp/Hack.zip && \
+    echo "Success!"
+
--- scripts/install_nerd_fonts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -fLo /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip && \
-    mkdir -p ~/.local/share/fonts/ && \
-    unzip /tmp/Hack.zip -d ~/.local/share/fonts/ && \
-    rm /tmp/Hack.zip && \
-    echo "Success!"
