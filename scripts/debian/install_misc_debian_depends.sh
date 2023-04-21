8debf129047124b85b63ba37fb840442f48dafc0
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-20 23:10:38 -0400

    Moved install depends script.

--- scripts/debian/install_misc_debian_depends.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget #TODO: git-delta
-# pacman -Syu
-# pacman -S bash-completion curl fd fzf gcc git git-delta gzip ripgrep unzip tar wget
---
+++ scripts/install_depends.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if package manager is pacman
+if [ "$(command -v pacman)" ]; then
+  sudo pacman -Syu
+  sudo pacman -S bash-completion curl fd fzf gcc git git-delta gzip ripgrep unzip tar wget
+# Check if package manager is apt
+elif [ "$(command -v apt)" ]; then
+  sudo apt update
+  sudo apt install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
+else
+  echo "Unknown package manager"
+  exit 1
+fi
