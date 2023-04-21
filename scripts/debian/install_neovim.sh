d512d4b4120233ee2b056b998ded473a10e563b7
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-20 23:30:55 -0400

    Merged more files.

--- scripts/debian/install_neovim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb -P /tmp/
-apt-get install -y /tmp/nvim-linux64.deb || true
-rm /tmp/nvim-linux64.deb
-
--- scripts/install_depends.sh
+++ scripts/install_depends.sh
@@ -2,16 +2,21 @@
 
 # Check if package manager is pacman
 if [ "$(command -v pacman)" ]; then
-  sudo pacman -Syu --noconfirm
-  sudo pacman -S --noconfirm bash-completion curl fd fzf gcc git git-delta gzip neovim ripgrep unzip tar wget
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm bash-completion curl fd fzf gcc git git-delta gzip neovim ripgrep unzip tar wget
 # Check if package manager is apt
 elif [ "$(command -v apt)" ]; then
-  sudo apt update
-  sudo apt install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
-  # TODO: Neovim, git-delta
+    sudo apt update
+    sudo apt install -y bash-completion curl g++ gcc fd-find fzf git gzip ripgrep unzip tar wget
+    # TODO: git-delta
+
+    # Install Neovim from GitHub because package manager might grab older version.
+    wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb -P /tmp/
+    apt-get install -y /tmp/nvim-linux64.deb || true
+    rm /tmp/nvim-linux64.deb
 else
-  echo "Unknown package manager"
-  exit 1
+    echo "Unknown package manager"
+    exit 1
 fi
 
 # Install nerd font to fonts directory in home folder.
