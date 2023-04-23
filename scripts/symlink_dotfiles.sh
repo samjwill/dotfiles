c261d20f2c4a9de82cf467c0b30332ebec673d6d
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-23 19:46:33 -0400

    Misc improvements.

--- scripts/symlink_dotfiles.sh
+++ scripts/symlink_dotfiles.sh
@@ -9,11 +9,14 @@ do
 
     destination_filepath=$(echo "$filepath" | cut -d':' -f1)
 
-    read -p "Replace $destination_filepath with a symlink pointing to $source_filepath ? [y/N] " -n 1 -r
+    read -p "Replace \"$destination_filepath\" with a symlink pointing to \"$source_filepath\"? [y/n]: " REPLY
     echo
 
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         rm -rf "${destination_filepath}" && ln -s "${source_filepath}" "${destination_filepath}"
+        echo "Done!"
+    else
+        echo "Skipping..."
     fi
 done
