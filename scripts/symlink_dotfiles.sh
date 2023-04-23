3438728454a9bc063e99fa97942a11507a0922ba
samjwill <25990267+samjwill@users.noreply.github.com>
2023-04-23 19:11:34 -0400

    Broke some stuff, but improving symlink script.

--- scripts/symlink_dotfiles.sh
+++ scripts/symlink_dotfiles.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-rm -f ~/.gitconfig && ln -s $(realpath ../.gitconfig) $(realpath ~/.gitconfig)
-rm -f ~/.bashrc && ln -s $(realpath ../.bashrc) $(realpath ~/.bashrc)
-rm -rf ~/.config/nvim && ln -s $(realpath ../.config/nvim) $(realpath ~/.config/nvim)
+
+FILEPATHS=(~/.gitconfig ~/.bashrc ~/.config/nvim)
+
+for destination_filepath in "${FILEPATHS[@]}"
+do
+    filename=${destination_filepath##*/}
+    filepath_in_repo=$(realpath "../${filename}")
+    read -p "Replace $destination_filepath with a symlink pointing to $filepath_in_repo? [y/N] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm -rf "${destination_filepath}" && ln -s "${filepath_in_repo}" "${destination_filepath}"
+    fi
+done
