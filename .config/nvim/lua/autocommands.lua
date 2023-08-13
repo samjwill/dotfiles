641ebd079b21f45da45765b34a0c69e65c63bf81
samjwill <25990267+samjwill@users.noreply.github.com>
2023-08-13 14:41:27 -0400

    Removed commented out autocmd.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -3,9 +3,3 @@ local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
 -- Turn off line numbers in terminal emulator
 vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
 
--- Automatically close terminal buffer when done.
--- vim.api.nvim_create_autocmd("TermClose", {group = init_group,command = "if !v:event.status | execute 'bdelete! ' . expand('<abuf>') | endif"})
--- TODO: This autocmd causes the error below...
--- Error detected while processing TermClose Autocommands for "*":
--- E516: No buffers were deleted: bdelete! 7
-
