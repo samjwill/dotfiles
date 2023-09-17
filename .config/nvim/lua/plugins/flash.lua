002a06b0ad465a55651c04eab4e73e4a178c1ad9
samjwill <25990267+samjwill@users.noreply.github.com>
2023-09-17 12:04:13 -0400

    Updates

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -3,11 +3,35 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-        { "s", mode = { "n", "o", "x" , "t"}, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "o", "x" , "t"}, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        {
+            "s",
+            mode = { "n", "o", "x", "t"},
+            function()
+                require("flash").jump()
+            end,
+            desc = "Flash"
+        },
+
+        {
+            "S",
+            mode = { "n", "o", "x", "t"},
+            function()
+                require("flash").treesitter()
+            end,
+            desc = "Flash Treesitter"
+        },
+
+        {
+            "<c-s>",
+            mode = { "c" },
+            function()
+                require("flash").toggle()
+            end,
+            desc = "Toggle Flash Search"
+        },
+
         -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
 
