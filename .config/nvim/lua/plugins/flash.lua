72653b0fecf2de2fb43cfa8aabdc3ef1a182308e
samjwill <25990267+samjwill@users.noreply.github.com>
2023-09-17 12:19:14 -0400

    Added TODOs.

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,11 +1,25 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        modes = {
+            char = {
+                keys = {
+                    -- TODO: Re-enable F/f/T/t when you figure out how to disable clever-f style repeat motions when pressing "f" a second time.
+                    -- TODO: Find a way to remove highlighting when a jump is made via F/f/T/t
+                },
+            },
+        },
+    },
     keys = {
         {
             "s",
-            mode = { "n", "o", "x", "t"},
+            -- TODO: Why doesn't this work in terminal-normal mode? Adding "t" here also forces it to be used in terminal-insert mode.
+            -- Even bringing this out doesn't have the desired effect because it breaks when you try to leave flash mode.
+            -- config = function()
+            --     vim.keymap.set("t", "s", function() require("flash").jump() end, {desc = "Flash"})
+            -- end
+            mode = { "n", "o", "x"},
             function()
                 require("flash").jump()
             end,
@@ -14,7 +28,7 @@ return {
 
         {
             "S",
-            mode = { "n", "o", "x", "t"},
+            mode = { "n", "o", "x"},
             function()
                 require("flash").treesitter()
             end,
@@ -33,5 +47,6 @@ return {
         -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     },
+
 }
 
