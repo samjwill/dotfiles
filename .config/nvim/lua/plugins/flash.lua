bb773a43ede1bb7f9ab9c9b18a1d712d51a69462
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-09-18 11:04:38 -0400

    Update flash.lua

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -47,15 +47,7 @@ return {
             desc = "Flash Treesitter"
         },
 
-        {
-            "<c-s>",
-            mode = { "c" },
-            function()
-                require("flash").toggle()
-            end,
-            desc = "Toggle Flash Search"
-        },
-
+        -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     },
