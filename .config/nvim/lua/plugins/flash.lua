7dd7aa6151e5b7f20d6e1b624f30a798252cdaa9
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-09-19 12:55:07 -0400

    Update flash.lua

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,5 +1,6 @@
 return {
     "folke/flash.nvim",
+    event = "VeryLazy", -- TODO: Not sure why this needs to be here, but if it's not, the settings in the config aren't taken consistently. Race condition?
     config = function()
         -- Makes the flash .jump() search look better with gruvbox-material "original"
         -- Otherwise, colors are too similar for the label and the highlighted search text
@@ -15,7 +16,8 @@ return {
                 },
                 char = {
                    -- TODO: Re-enable F/f/T/t when you figure out how to disable clever-f style repeat motions when pressing "f" a second time.
-                   -- TODO: Find a way to remove highlighting when a jump is made via F/f/T/t
+                   -- See https://github.com/folke/flash.nvim/issues/245
+                   -- TODO: Find a way to remove highlighting when a jump is made via F/f/T/t.
                     enabled = false,
                 },
             },
