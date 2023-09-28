b777e11c1bfab47c775036572e9ccc83c7db474d
samjwill <25990267+samjwill@users.noreply.github.com>
2023-09-28 01:37:08 -0400

    Updated indent blankline.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,7 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-        require('indent_blankline').setup{}
-        vim.g.indent_blankline_show_trailing_blankline_indent = false
-        -- See: https://github.com/lukas-reineke/indent-blankline.nvim/issues/489#issuecomment-1276972280
-        vim.api.nvim_create_autocmd("CursorMoved", {command = "IndentBlanklineRefresh"})
+        require("ibl").setup()
     end
 }
 
