b2cf4b08922bded9306e8070f4e2bf9d0b571f25
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-09-28 15:51:44 -0400

    Update flash.lua

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,25 +1,59 @@
 return {
     "folke/flash.nvim",
-    -- dependencies = {"chrisbra/improvedft"}, -- Using this for the F/f/T/t motions instead of flash until either https://github.com/folke/flash.nvim/issues/245 or https://github.com/folke/flash.nvim/issues/231 are resolved.
     event = "VeryLazy", -- TODO: Not certain why this needs to be here, but if it's not, the settings in the config aren't taken consistently. Race condition, or maybe intentionally designed like this?
     config = function()
-        -- Makes the flash .jump() search look better with gruvbox-material "original"
-        -- Otherwise, colors are too similar for the label and the highlighted search text
-        vim.cmd("highlight clear FlashLabel")
-        vim.cmd("highlight link FlashLabel Error")
-        vim.cmd("highlight clear FlashCurrent")
-        vim.cmd("highlight link FlashCurrent Substitute")
+
+        -- Cursor is basically impossible to see when using F/f/T/t motions with gruvbox colorscheme if you don't tweak the cursor highlight colors.
+        -- Not 100% sure what's going on, but it really only appears to affect the cursor color in operator-pending mode.
+        local function get_highlight_attributes(group_name)
+            local hl_id = vim.fn.hlID(group_name)
+            local guifg = vim.fn.synIDattr(hl_id, "fg#")
+            local guibg = vim.fn.synIDattr(hl_id, "bg#")
+            local ctermfg = vim.fn.synIDattr(hl_id, "fg", "cterm")
+            local ctermbg = vim.fn.synIDattr(hl_id, "bg", "cterm")
+            return guifg, guibg, ctermfg, ctermbg
+        end
+        local function set_cursor_highlight_to_incsearch()
+            local incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg = get_highlight_attributes("IncSearch")
+            vim.cmd(string.format("highlight Cursor guifg=%s guibg=%s ctermfg=%s ctermbg=%s", incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg))
+        end
+        set_cursor_highlight_to_incsearch()
 
         require("flash").setup {
+            highlight = {
+                groups = {
+                    -- Makes distinguishing between labels and matches easier when jumping.
+                    match = "Search",
+                    current = "Substitute",
+                    backdrop = "FlashBackdrop",
+                    label = "Error",
+                },
+            },
             modes = {
                 search = {
                     enabled = false,
                 },
                 char = {
-                   -- TODO: Re-enable F/f/T/t when you figure out how to disable clever-f style repeat motions when pressing "f" a second time.
-                   -- See https://github.com/folke/flash.nvim/issues/245
-                   -- TODO: Find a way to remove highlighting when a jump is made via F/f/T/t.
-                    enabled = false,
+                    char_actions = function()
+                        return {
+                            [";"] = "next",
+                            [","] = "prev",
+                        }
+                    end,
+                    highlight = {
+                        -- show a backdrop with hl FlashBackdrop
+                        backdrop = false,
+                        -- Highlight the search matches
+                        matches = true,
+                        -- extmark priority
+                        priority = 5000,
+                        groups = {
+                            match = "IncSearch",
+                            current = "IncSearch",
+                            backdrop = "FlashBackdrop",
+                            label = "Search",
+                        },
+                    },
                 },
             },
         }
