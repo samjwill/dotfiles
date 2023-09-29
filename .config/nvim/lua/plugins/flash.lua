fceb3f1ada1d6a859ee1f26beae767c0777c1e36
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-09-29 10:46:08 -0400

    Disabled flash F/f/T/t because colorscheme wouldn't cooperate with it.

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,33 +1,32 @@
 return {
     "folke/flash.nvim",
-    dependencies = {"rebelot/kanagawa.nvim"},
     event = "VeryLazy", -- TODO: Not certain why this needs to be here, but if it's not, the settings in the config aren't taken consistently. Race condition, or maybe intentionally designed like this?
     config = function()
 
-        -- Cursor is basically impossible to see when using F/f/T/t motions with gruvbox colorscheme if you don't tweak the cursor highlight colors.
-        -- Not 100% sure what's going on, but it really only appears to affect the cursor color in operator-pending mode.
-        local function get_highlight_attributes(group_name)
-            local hl_id = vim.fn.hlID(group_name)
-            local guifg = vim.fn.synIDattr(hl_id, "fg#")
-            local guibg = vim.fn.synIDattr(hl_id, "bg#")
-            local ctermfg = vim.fn.synIDattr(hl_id, "fg", "cterm")
-            local ctermbg = vim.fn.synIDattr(hl_id, "bg", "cterm")
-            return guifg, guibg, ctermfg, ctermbg
-        end
-
-        local function make_termcursor_independent()
-            vim.cmd(string.format("highlight clear TermCursor"))
-            vim.cmd(string.format("highlight clear TermCursorNC"))
-            vim.cmd(string.format("highlight TermCursor cterm=reverse gui=reverse"))
-            vim.cmd(string.format("highlight TermCursorNC cterm=reverse gui=reverse"))
-        end
-        make_termcursor_independent() -- Termcursor is linked to this, which makes it invisible in terminal buffers otherwise.
-
-        local function set_cursor_highlight_to_incsearch()
-            local incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg = get_highlight_attributes("IncSearch")
-            vim.cmd(string.format("highlight Cursor guifg=%s guibg=%s ctermfg=%s ctermbg=%s", incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg))
-        end
-        set_cursor_highlight_to_incsearch()
+        -- -- Cursor is basically impossible to see when using F/f/T/t motions with gruvbox colorscheme if you don't tweak the cursor highlight colors.
+        -- -- Not 100% sure what's going on, but it really only appears to affect the cursor color in operator-pending mode.
+        -- local function get_highlight_attributes(group_name)
+        --     local hl_id = vim.fn.hlID(group_name)
+        --     local guifg = vim.fn.synIDattr(hl_id, "fg#")
+        --     local guibg = vim.fn.synIDattr(hl_id, "bg#")
+        --     local ctermfg = vim.fn.synIDattr(hl_id, "fg", "cterm")
+        --     local ctermbg = vim.fn.synIDattr(hl_id, "bg", "cterm")
+        --     return guifg, guibg, ctermfg, ctermbg
+        -- end
+        --
+        -- local function make_termcursor_independent()
+        --     vim.cmd(string.format("highlight clear TermCursor"))
+        --     vim.cmd(string.format("highlight clear TermCursorNC"))
+        --     vim.cmd(string.format("highlight TermCursor cterm=reverse gui=reverse"))
+        --     vim.cmd(string.format("highlight TermCursorNC cterm=reverse gui=reverse"))
+        -- end
+        -- make_termcursor_independent() -- Termcursor is linked to this, which makes it invisible in terminal buffers otherwise.
+        --
+        -- local function set_cursor_highlight_to_incsearch()
+        --     local incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg = get_highlight_attributes("IncSearch")
+        --     vim.cmd(string.format("highlight Cursor guifg=%s guibg=%s ctermfg=%s ctermbg=%s", incsearch_guifg, incsearch_guibg, incsearch_ctermfg, incsearch_ctermbg))
+        -- end
+        -- set_cursor_highlight_to_incsearch()
 
         require("flash").setup {
             highlight = {
@@ -44,6 +43,7 @@ return {
                     enabled = false,
                 },
                 char = {
+                    enabled = false, -- TODO: Re-enable when colorscheme cooperates with flash.
                     char_actions = function()
                         return {
                             [";"] = "next",
