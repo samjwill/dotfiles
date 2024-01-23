4f3627288148bc514bef6cc8ef9a3837d94e5ad9
samjwill <25990267+samjwill@users.noreply.github.com>
2024-01-23 05:09:48 -0500

    Exit on error.

--- scripts/install_depends.sh
+++ scripts/install_depends.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 # A script to install the programs that my dotfiles expect to be present on the system.
+
+# Exit on error.
+set -e
 
 # Check if package manager is pacman
 if [ "$(command -v pacman)" ]; then
+    # Perform a system update
     sudo pacman -Syu --noconfirm
+
     # Package names as they appear on Arch's default repositories.
     sudo pacman -S --noconfirm --needed \
         bash-completion \
