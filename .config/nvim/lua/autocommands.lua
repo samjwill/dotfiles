4cebcb64fe0c0760810646d0840c9da5f58201c6
samjwill <25990267+samjwill@users.noreply.github.com>
2024-02-03 18:00:48 -0500

    Quit iff you closed the terminal and the terminal was the last open buffer.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -3,12 +3,23 @@ local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
 -- Turn off line numbers in terminal emulator
 vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
 
--- Automatically press some key to dismiss "[Process exited 0]"
--- Need to use feedkeys because of https://github.com/neovim/neovim/issues/4895
 vim.api.nvim_create_autocmd("TermClose",
     {
         group = init_group,
         callback = function()
+            -- Check if we just closed the last open window.
+            local should_exit = false
+            local num_tabs_open = vim.fn.tabpagenr('$')
+            if (num_tabs_open == 1) then
+                local num_windows_open = #vim.fn.tabpagebuflist()
+                if (num_windows_open == 1) then
+                    should_exit = true
+                end
+            end
+
+            -- Automatically press some key to dismiss "[Process exited 0]"
+            -- Need to use feedkeys because of https://github.com/neovim/neovim/issues/4895
+            --
             -- Plugins like unception can delete a terminal buffer when opening
             -- a new file. This is expected, but that means that without
             -- filtering this callback, it can trigger when not desired, and we
@@ -23,6 +34,10 @@ vim.api.nvim_create_autocmd("TermClose",
             -- confuse flash.nvim. This is what I came up with...
             local escape_key = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
             vim.api.nvim_feedkeys(escape_key, 'n', false)
+
+            if (should_exit) then
+                vim.cmd("quit")
+            end
         end
     }
 )
