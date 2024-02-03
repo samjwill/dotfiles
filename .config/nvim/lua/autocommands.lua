e4d27da9d470b4d5392916a1c5eb7131fefed2a5
samjwill <25990267+samjwill@users.noreply.github.com>
2024-02-03 17:20:27 -0500

    Needed a more complicated workaround.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -9,8 +9,20 @@ vim.api.nvim_create_autocmd("TermClose",
     {
         group = init_group,
         callback = function()
-            -- Key pressed is arbitrary.
-            vim.fn.feedkeys("i")
+            -- Plugins like unception can delete a terminal buffer when opening
+            -- a new file. This is expected, but that means that without
+            -- filtering this callback, it can trigger when not desired, and we
+            -- don't appear to have the ability to try to get the buftype to
+            -- filter it due to the issue linked above.
+            --
+            -- As a result, we need to just make sure that the keys fed not do
+            -- anything. Feeding "jk" won't work, because if j is pressed, it
+            -- may or may not close the terminal buffer, and then "k" will be
+            -- feed on the following buffer iff j dismisses the finished
+            -- terminal buffer. Trying to execute feedkeys "\\<Esc>" appears to
+            -- confuse flash.nvim. This is what I came up with...
+            local escape_key = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
+            vim.api.nvim_feedkeys(escape_key, 'n', false)
         end
     }
 )
