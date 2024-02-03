05c9c1f03537cc8a4d723f7a0a1df3d6e95b1d32
samjwill <25990267+samjwill@users.noreply.github.com>
2024-02-03 17:03:07 -0500

    Added autocommand for termclose.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -2,3 +2,15 @@ local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
 
 -- Turn off line numbers in terminal emulator
 vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
+
+-- Automatically press some key to dismiss "[Process exited 0]"
+-- Need to use feedkeys because of https://github.com/neovim/neovim/issues/4895
+vim.api.nvim_create_autocmd("TermClose",
+    {
+        group = init_group,
+        callback = function()
+            -- Key pressed is arbitrary.
+            vim.fn.feedkeys("i")
+        end
+    }
+)
