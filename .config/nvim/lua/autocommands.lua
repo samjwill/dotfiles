d0e0cdda06c86468698446c696f3ea68a6e0d22a
samjwill <25990267+samjwill@users.noreply.github.com>
2024-02-04 13:37:18 -0500

    Moved comment.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -3,11 +3,12 @@ local init_group = vim.api.nvim_create_augroup("INIT_CMDS", {clear = true})
 -- Turn off line numbers in terminal emulator
 vim.api.nvim_create_autocmd("TermOpen", {group = init_group, command = "setlocal nonumber"})
 
+-- Automatically press some key to dismiss "[Process exited 0]", and close nvim
+-- if the terminal buffer that closed was the only unhidden, unmodified buffer.
 vim.api.nvim_create_autocmd("TermClose",
     {
         group = init_group,
         callback = function()
-            -- Automatically press some key to dismiss "[Process exited 0]"
             -- Need to use feedkeys because of https://github.com/neovim/neovim/issues/4895
             --
             -- Plugins like unception can delete a terminal buffer when opening
