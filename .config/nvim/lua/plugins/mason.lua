f795179b6b747384cf8178e5f8ba4eec9b1bd31b
samjwill <25990267+samjwill@users.noreply.github.com>
2025-04-19 21:18:45 -0400

    Fix issue with auto installing mason plugins

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,6 +2,8 @@
 -- cpptools for dap
 local mason_package_list = { "clangd", "cpptools" }
 
+-- Use a global flag to signal that a build/update occurred.
+
 -- At the time of writing, mason expects the following packages to be installed.
 -- See https://github.com/williamboman/mason.nvim for a list of up-to-date dependencies.
 -- * git
@@ -12,17 +14,21 @@ local mason_package_list = { "clangd", "cpptools" }
 
 return {
     "williamboman/mason.nvim",
+
     -- "build" is executed when a plugin is installed or updated.
     build = function()
-        -- TODO: This causes GUI weirdness if Neovim is currently being
-        -- bootstrapped. Leaving in place because at least this installs
-        -- packages automatically on launch if they haven't been installed
-        -- already.
-        vim.cmd("MasonInstall "..table.concat(mason_package_list, " "))
+        _G.mason_run_install_post_build = true
     end,
+
     -- "config" is executed when the plugin loads.
     config = function()
         require("mason").setup()
+        if _G.mason_run_install_post_build then
+            vim.schedule(function()
+                vim.cmd("MasonInstall " .. table.concat(mason_package_list, " "))
+            end)
+            -- Reset the flag after handling installation.
+        end
 
         -- Create a command that allows for simple, headless installation.
         vim.api.nvim_create_user_command("MasonInstallAll", function ()
