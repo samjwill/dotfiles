2d2b3ea2805388e39338c7a20c7056f1e0c7f4ea
samjwill <25990267+samjwill@users.noreply.github.com>
2025-04-19 20:30:58 -0400

    Replaced blacklist with whitelist for treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,49 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
-        -- See: https://github.com/tree-sitter/tree-sitter-css/issues/35
-        local ignore_list = {
-            "css",
-            "help",
-            "smali",
-        }
         require("nvim-treesitter.configs").setup({
-            ensure_installed = "all",
-            ignore_install = ignore_list,
+            ensure_installed = {
+                "bash",
+                "c",
+                "c_sharp",
+                "cmake",
+                "comment",
+                "cpp",
+                "css",
+                "csv",
+                "diff",
+                "dockerfile",
+                "doxygen",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "groovy",
+                "html",
+                "java",
+                "javascript",
+                "json",
+                "julia",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "ninja",
+                "perl",
+                "python",
+                "r",
+                "rust",
+                "sql",
+                "tcl",
+                "toml",
+                "typescript",
+                "typst",
+                "vim",
+                "xml",
+                "yaml",
+            },
 
             -- Only sync_install if running headless.
             -- From: https://github.com/nvim-treesitter/nvim-treesitter/issues/3579#issuecomment-1278662119
@@ -17,8 +51,6 @@ return {
 
             highlight = {
                 enable = true,
-                -- Treesitter help page highlighting has broken before. The default highlighting's fine.
-                disable = ignore_list,
             }
         })
 
