10cbb6df086bd24663d374a260d2cd9f544c3dfc
samjwill <25990267+samjwill@users.noreply.github.com>
2026-04-02 22:27:39 -0400

    Added error logs

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,23 @@
--- At the time of writing, mason expects the following packages to be installed.
--- See https://github.com/williamboman/mason.nvim for a list of up-to-date dependencies.
--- * git
--- * curl or wget
--- * unzip
--- * tar or gtar
--- * gzip
+-- See: https://github.com/mason-org/mason.nvim?tab=readme-ov-file#requirements
+if vim.fn.executable("git") == 0 then
+    vim.notify("Missing external dependency for mason: git", vim.log.levels.ERROR)
+end
+
+if vim.fn.executable("curl") == 0 and vim.fn.executable("wget") == 0 then
+    vim.notify("Missing external dependency for mason: curl or wget", vim.log.levels.ERROR)
+end
+
+if vim.fn.executable("unzip") == 0 then
+    vim.notify("Missing external dependency for mason: unzip", vim.log.levels.ERROR)
+end
+
+if vim.fn.executable("tar") == 0 and vim.fn.executable("gtar") == 0 then
+    vim.notify("Missing external dependency for mason: tar or gtar", vim.log.levels.ERROR)
+end
+
+if vim.fn.executable("gzip") == 0 then
+    vim.notify("Missing external dependency for mason: gzip", vim.log.levels.ERROR)
+end
 
 vim.pack.add(
     {
